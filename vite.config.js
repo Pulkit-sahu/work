928791dfb595d8594141fb3e14bce35dc6448ab4
Pulkit-sahu <pulkit250402@gmail.com>
2024-03-14 01:20:02 +0530

    add: initial files

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base:'/',
+  base:'/traya/',
   plugins: [
     {
       name: 'treat-js-files-as-jsx',
